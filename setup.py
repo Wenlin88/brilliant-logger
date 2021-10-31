3eb2293c7394bbff8325f948a2203a6548f307c4
Henri Wenlin <henri.wenlin@kone.com>
2021-10-31 16:11:41 +0200

    setup.py päivitetty vastaamaan tätä pakettia

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='pyv1',
-      version='0.0.3',
-      description='This tool is created to help everyday work with KONE VersionOne Agile portfolio and project management tool',
-      url='http://kcofilxkqd001:8888/Henkka/pyv1',
+setup(name='easylogging',
+      version='0.0.1',
+      description="Henkka's python package for making python logging easy",
+      url='https://github.com/Wenlin88/easylogging',
       author='Henri Wenlin',
-      author_email='henri.wenlin@kone.com',
-      license='Private, internal software',
+      author_email='henri.wenlin@outlook.com',
+      license='GNU GPLv3',
       packages=find_packages(),
-      package_data = {"": ["*.ipynb"],},
       install_requires=[
       "wheel>=0.35.1",      
-      "requests>=2.25.0",
-      "keyring >= 23.1.0",
-      "pandas >= 1.3.2",
-      "notebook >= 6.4.3",
-      "jupyter-wysiwyg >= 19.10",
       ],
       classifiers=['Programming Language :: Python :: 3'],
       zip_safe=False)
